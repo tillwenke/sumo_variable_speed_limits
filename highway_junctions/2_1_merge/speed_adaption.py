d08e6fe81de2c1b85da5db51169a1adb8bff5688
Till Wenke <till.wenke@gmx.net>
2022-10-28 15:41:06 +0300

    metrics and speed adaption for 2 to 1

--- highway_junctions/2_1_merge/speed_adaption.py
+++ highway_junctions/2_1_merge/speed_adaption.py
@@ -1,5 +1,7 @@
 import os, sys
+from statistics import mean
 import traci
+import numpy as np
 
 
 # from https://sumo.dlr.de/docs/TraCI/Interfacing_TraCI_from_Python.html
@@ -22,72 +24,146 @@ sumoCmd = [sumoBinary, "-c", "2_1_merge.sumocfg", '--start']
 #run sumo simulation
 traci.start(sumoCmd)
 
+
 # parameters and sumo objects
 
-
 # define sumo objects
-loop_before_top = "loop_seg_0_before_1"
-loop_before_top = "loop_seg_0_before_0"
-loop_after = "loop_seg_0_after_0"
+# lane 0 is at bottom
+segment_length = 50 #meters
 
-seg_0_before_top = "seg_0_before_0"
+point_loops_before = ["point_loop_seg_0_before_1", "point_loop_seg_0_before_0"]
+segment_loops_before =["segment_loop_seg_0_before_1", "segment_loop_seg_0_before_0"]
+
+point_loops_after = ["point_loop_seg_0_after_0"]
+segment_loops_after = ["segment_loop_seg_0_after_0"]
+
+# need those?
 seg_0_before_bottom = "seg_0_before_1"
+seg_0_before_top = "seg_0_before_0"
 seg_0_after = "seg_0_after_0"
+
+traci.lane.setMaxSpeed(seg_0_before_top, 0.5)
+traci.lane.setMaxSpeed(seg_0_before_bottom, 1)
+
 
 # define metrics
 density = 0
-veh_sum = 0
-
 flow = 0
-time_mean_speed = 0
-space_mean_speed = 0
+mean_speed = 0
 
+# accumulators
+veh_on_sum = 0
+veh_entered_sum = 0
+mean_speed_on_sum = 0
 
+veh_on_sum_b = 0
+veh_entered_sum_b = 0
+mean_speed_on_sum_b = 0
+
+# variable speed limit
 low_speed = 1
 high_speed = 10
 
-
+# simulation
 step = 0
-speed_sum = 0
+aggregation_time = 100 #seconds - always aggregate the last 100 step to make decision in the present
+print('AVERAGED OVER', aggregation_time, 'STEPS')
 
-aggregation_time = 100 # always aggregate the last 100 step to make decision in the present
 
 # run till all cars are gone
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep() 
     step += 1
 
-    this_num_veh =  traci.inductionloop.getLastStepVehicleNumber(loop_after)
-    
+    # detection after
+    veh_on = sum([traci.inductionloop.getLastStepVehicleNumber(loop) for loop in segment_loops_after])
+    veh_on_sum += veh_on
 
-    veh_sum += this_num_veh
+    veh_entered = sum([traci.inductionloop.getLastStepVehicleNumber(loop) for loop in point_loops_after])
+    veh_entered_sum += veh_entered
 
-    this_step_speed = traci.inductionloop.getLastStepMeanSpeed(loop_after)
-    if this_step_speed >= 0:
-        speed_sum += this_step_speed
+    # "on" the induction loop
+    mean_speed_on = sum([traci.inductionloop.getLastStepMeanSpeed(loop) for loop in point_loops_after]) / len(point_loops_after)
+    # speed if -1 if no cars pass
+    if mean_speed_on >= 0:
+        mean_speed_on_sum += mean_speed_on
 
+    # detection before
+    veh_on_b = sum([traci.inductionloop.getLastStepVehicleNumber(loop) for loop in segment_loops_before])
+    veh_on_sum_b += veh_on_b
 
+    veh_entered_b = sum([traci.inductionloop.getLastStepVehicleNumber(loop) for loop in point_loops_before])
+    veh_entered_sum_b += veh_entered_b
+
+    # "on" the induction loop
+    mean_speed_on_b = sum([traci.inductionloop.getLastStepMeanSpeed(loop) for loop in point_loops_after]) / len(point_loops_before)
+    # speed if -1 if no cars pass
+    if mean_speed_on_b >= 0:
+        mean_speed_on_sum_b += mean_speed_on_b
+
+    # output
+    # sanity check against the XML output
+    """
     if step < 100:
-        print(step-1, this_num_veh, '|', this_step_speed)
+        print(step-1, veh_on, '|', mean_speed_on)
+    """
     
     if step % aggregation_time == 0:
+        
+        # after
+        avg_num_veh_on = veh_on_sum / aggregation_time
+        density = avg_num_veh_on / segment_length
 
-        print(veh_sum, '/', aggregation_time, 'steps')
-        veh_sum = 0
+        density_out = density
+            
+        print('density', round(density,3), 'veh/m ', end='; ')
 
-        """
-        speed_avg = speed_sum / cycle_time
-        speed_sum = 0
+        flow = veh_entered_sum / aggregation_time 
+        # the point_loop should act as point detector
+        # not expected to have one single car on it for more than 1 time step
+        print('flow', flow, 'veh/s', end='; ')
+        
+        # one speed metric is enough
+        # this equals to spot speed as induction loop is short
+        mean_speed = mean_speed_on_sum / aggregation_time
+        print('speed', round(mean_speed,2), 'm/s', end='; ')
 
-        if speed_avg > 2:
-            traci.lane.setMaxSpeed(lane_1_id, high_speed)
-            traci.lane.setMaxSpeed(lane_2_id, high_speed)
-        else:
-            traci.lane.setMaxSpeed(lane_1_id, low_speed)
-            traci.lane.setMaxSpeed(lane_2_id, low_speed)
+        # before
+        avg_num_veh_on = veh_on_sum_b / aggregation_time
+        density = avg_num_veh_on / segment_length
 
-        print(speed_avg, traci.inductionloop.getSubscriptionResults(loop_id), traci.inductionloop.getLastStepMeanSpeed(loop_id))
-        """
+        density_in = density
+            
+        print('density', round(density,3), 'veh/m ', end='; ')
 
+        flow = veh_entered_sum_b / aggregation_time 
+        # the point_loop should act as point detector
+        # not expected to have one single car on it for more than 1 time step
+        print('flow', flow, 'veh/s', end='; ')
+        
+        # one speed metric is enough
+        # this equals to spot speed as induction loop is short
+        mean_speed = mean_speed_on_sum_b / aggregation_time
+        print('speed', round(mean_speed,2), 'm/s')
+        
+        # adapt the speed limit to make traffic fluent
+        if density_out < density_in:
+            top = traci.lane.getMaxSpeed(seg_0_before_top)
+            bottom = traci.lane.getMaxSpeed(seg_0_before_bottom)
+
+            traci.lane.setMaxSpeed(seg_0_before_top, top + 0.1)
+            traci.lane.setMaxSpeed(seg_0_before_bottom, bottom + 0.1)
+
+        # reset accum
+        veh_on_sum = 0
+        veh_entered_sum = 0
+        mean_speed_on_sum = 0
+
+        veh_on_sum_b = 0
+        veh_entered_sum_b = 0
+        mean_speed_on_sum_b = 0
+
+
+        
 
 traci.close()
