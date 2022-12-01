0116a63cd5e095e385a0f112a502f8a965240bbd
Till Wenke <till.wenke@gmx.net>
2022-12-01 23:19:23 +0200

    modularize rl section

--- highway_junctions/rl/rl_test.py
+++ highway_junctions/rl/rl_test.py
@@ -15,16 +15,19 @@ from rl.agents import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
-import rl.rl_occ_control as rl_occ_control
+import gym_envs
 import rl_utils
 
-env = rl_occ_control.SUMOEnv()
+env = gym_envs.speed_SUMOEnv()
 actions = env.action_space.n
-states = env.observation_space.shape[0]
+states = env.observation_space.shape
 model = rl_utils.build_model(states, actions)
 dqn = rl_utils.build_agent(model, actions)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 dqn.load_weights('dqn_weights.h5f')
 
-_ = dqn.test(env, nb_episodes=5, visualize=True)
+_ = dqn.test(env, nb_episodes=1, visualize=False)
+
+plt.plot(env.mean_speeds)
+plt.show()
